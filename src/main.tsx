e08bbcfa8ad4bca1f93a5d16acccfd98dfb689bb
gabellon-ai <gregbellon0@gmail.com>
2025-08-17 13:21:35 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'     // no extension; resolves App.tsx
+import App from './App'        // resolves App.tsx
 import './index.css'
 
+console.log('main.tsx booted')
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
