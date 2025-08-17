f28b191998fdcf8cbf479690d41b93939e348b6a
gabellon-ai <gregbellon0@gmail.com>
2025-08-17 12:42:53 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,3 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/ishares-report/',   // repo name
-  plugins: [react()],
-})
+export default defineConfig({ base: '/', plugins: [react()] })
