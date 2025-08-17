3024ff894560e7f5e58473b55e7aa0b4da121d8c
gabellon-ai <gregbellon0@gmail.com>
2025-08-17 15:00:56 -0500

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,6 @@
-// tailwind.config.cjs
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: { extend: {} },
-  plugins: [], // add require("tailwindcss-animate") only if you've installed it
+  plugins: [],
 };
