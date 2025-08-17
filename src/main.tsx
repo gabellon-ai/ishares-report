f23949554f4f5bac24d78a2a939aa2cb4697eab2
gabellon-ai <gregbellon0@gmail.com>
2025-08-17 14:22:55 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";               // ⬅️ this line is REQUIRED
+import "./index.css";              // ← REQUIRED
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
