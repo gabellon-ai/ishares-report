9dd4afdf74cf286944a3eea4a74c5a066cf93388
gabellon-ai <gregbellon0@gmail.com>
2025-08-17 13:31:21 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+// src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'        // resolves App.tsx
+import App from './App'          // resolves App.tsx
 import './index.css'
 
-console.log('main.tsx booted')
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootEl = document.getElementById('root')
+if (!rootEl) throw new Error('Missing <div id="root"></div> in index.html')
+
+ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
