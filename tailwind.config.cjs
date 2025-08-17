6a45faeafbf1321ade08f9cb8ef5feb0f09b942f
gabellon-ai <gregbellon0@gmail.com>
2025-08-17 14:31:46 -0500

    Update and rename tailwind.config.js to tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,7 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: ["class"],
+module.exports = {
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: { extend: {} },
-  plugins: [require("tailwindcss-animate")], // optional
+  plugins: [require("tailwindcss-animate")],
 };
